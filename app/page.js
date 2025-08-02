f9d46e6852a8b1e6a9ccb1ed591a7cc06409e344
Shreevathsa05 <bhatshreevathsa17@gmail.com>
2025-08-02 18:54:27 +0530

    text color changed

--- app/page.js
+++ app/page.js
@@ -9,7 +9,7 @@ export default function Home() {
       <Hero/>
      </div>
      <div className="mt-[10vh]">
-      <div className="text-3xl pb-16 md:text-4xl text-center text-purple-300">Trending</div>
+      <div className="text-3xl pb-16 md:text-4xl text-center text-cyan-400">Trending</div>
       <Marquee/>
      </div>
     </div>
